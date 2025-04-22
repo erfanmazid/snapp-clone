0f42b129a0814e8f9089f1b10ebe8c4e5f27269e
erfanmazid <erfanmazidm@gmail.com>
2025-04-22 20:45:20 +0330

    feat: create hook for get user data

---
+++ hooks/useUserById/type.ts
@@ -0,0 +1,56 @@
+export interface UserLogin {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  expires_at: number;
+  refresh_token: string;
+  user: User;
+}
+export interface User {
+  id: string;
+  aud: string;
+  role: string;
+  email: string;
+  email_confirmed_at: string;
+  phone: string;
+  confirmation_sent_at: string;
+  confirmed_at: string;
+  last_sign_in_at: string;
+  app_metadata: AppMetadata;
+  user_metadata: IdentityDataOrUserMetadata;
+  identities?: IdentitiesEntity[] | null;
+  created_at: string;
+  updated_at: string;
+  is_anonymous: boolean;
+}
+export interface AppMetadata {
+  provider: string;
+  providers?: string[] | null;
+}
+export interface IdentityDataOrUserMetadata {
+  email: string;
+  email_verified: boolean;
+  phone_verified: boolean;
+  sub: string;
+}
+export interface IdentitiesEntity {
+  identity_id: string;
+  id: string;
+  user_id: string;
+  identity_data: IdentityDataOrUserMetadata;
+  provider: string;
+  last_sign_in_at: string;
+  created_at: string;
+  updated_at: string;
+  email: string;
+}
+
+export interface UserDate {
+  id: string;
+  full_name: string;
+  phone: string;
+  email: string;
+  role: string;
+  profile_image?: null;
+  created_at: string;
+}
---
+++ hooks/useUserById/useUserById.ts
@@ -0,0 +1,37 @@
+// hooks/useUserById.ts
+import { useEffect, useState } from "react";
+import { supabase } from "@/lib/supabaseClient";
+import { UserDate } from "./type";
+
+export function useUserById(userId: string | null) {
+  const [user, setUser] = useState<UserDate | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!userId) return;
+
+    const fetchUser = async () => {
+      setLoading(true);
+      const { data, error } = await supabase
+        .from("users")
+        .select("*")
+        .eq("id", userId)
+        .single();
+
+      if (error) {
+        setError(error.message);
+        setUser(null);
+      } else {
+        setUser(data);
+        setError(null);
+      }
+
+      setLoading(false);
+    };
+
+    fetchUser();
+  }, [userId]);
+
+  return { user, loading, error };
+}
