b9b18942890e1a1303e311feefdb00c89ab80ac7
erfanmazid <erfanmazidm@gmail.com>
2025-04-22 15:53:13 +0330

    feat: add login and register and ... hooks

---
+++ hooks/useAuth.ts
@@ -0,0 +1,72 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { supabase } from "@/lib/supabaseClient";
+import { User } from "@supabase/supabase-js";
+
+export const useAuth = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getSession = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+      setUser(session?.user ?? null);
+      setLoading(false);
+    };
+
+    getSession();
+
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setUser(session?.user ?? null);
+      }
+    );
+
+    return () => {
+      listener.subscription.unsubscribe();
+    };
+  }, []);
+
+  const signUp = async (
+    email: string,
+    password: string,
+    emailRedirectTo: string
+  ) => {
+    setLoading(true);
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: { emailRedirectTo: emailRedirectTo },
+    });
+    setLoading(false);
+    return { data, error };
+  };
+
+  const signIn = async (email: string, password: string) => {
+    setLoading(true);
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    setLoading(false);
+    return { data, error };
+  };
+
+  const signOut = async () => {
+    setLoading(true);
+    const { error } = await supabase.auth.signOut();
+    setLoading(false);
+    return { error };
+  };
+
+  return {
+    user,
+    loading,
+    signUp,
+    signIn,
+    signOut,
+  };
+};
